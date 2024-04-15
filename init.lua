108015e4f8dbc0cf98b4fc6286343411b4d9cb33
Michael Terranova <mjt@Michaels-MacBook-Air.local>
2024-04-14 21:21:16 -0700

    Init nvim config

---
+++ init.lua
@@ -0,0 +1,37 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
